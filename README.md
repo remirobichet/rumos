c143aef05bfa0adf84b6dc1dff6f5b005aa84858
Rémi Robichet <remi.robichet@edtake.com>
2023-05-12 10:22:35 +0200

    feat: init rumos 🎉

---
+++ README.md
@@ -0,0 +1,16 @@
+# rumos
+
+> CLI tool to run faster monorepo scripts
+
+# Install
+
+Install moru as dev dependancy
+
+`pnpm -w add rumos --save-dev`
+
+
+# Usage
+
+In your workspace
+
+`pnpm rumos`
---
+++ index.js
@@ -0,0 +1,79 @@
+#! /usr/bin/env node
+
+import enquirer from 'enquirer'
+import { exec, execSync } from 'child_process'
+import path from 'path'
+
+const __dirname = process.cwd()
+const { prompt } = enquirer
+
+/* List pnpm workspaces - https://github.com/pnpm/pnpm/issues/1519#issuecomment-1299922699 */
+const workspacesObjects = await new Promise((resolve, reject) => {
+  exec('pnpm m ls --json --depth=-1', (error, stdout, stderr) => {
+    if (error) {
+      reject(error)
+      return;
+    }
+    if (stderr) {
+      reject(error)
+      return;
+    }
+    resolve(JSON.parse(stdout));
+  });
+})
+const workspacesPath = workspacesObjects.map(object => path.relative(__dirname, object.path))
+
+const workspacesGroups = workspacesPath.reduce((acc, path) => {
+  if (!path.includes('/')) {
+    if (path === '') {
+      acc['root'] = ['']
+    } else {
+      acc[path] = ['']
+    }
+  } else {
+    let [folder, name] = path.split('/');
+    if (acc[folder]) {
+      acc[folder].push(name);
+    } else
+      acc[folder] = [name];
+  }
+  return acc
+}, {})
+
+const workspaceChoices = Object.keys(workspacesGroups).reduce((acc, group, i, arr) => {
+  if (i !== 0) {
+    acc.push({ message: ` --- ${group.charAt(0).toUpperCase() + group.slice(1)} --- `, role: 'separator' })
+  }
+  if (group === 'root') {
+    acc.push({ message: 'Root', value: '/' })
+  } else {
+    workspacesGroups[group].forEach(el => acc.push({ message: el, value: `/${group}/${el}` }))
+  }
+  return acc
+}, [])
+
+const { workspace } = await prompt({
+  type: 'select',
+  name: 'workspace',
+  message: 'Select workspace to run',
+  choices: workspaceChoices
+})
+
+let isRoot = false
+if (workspace === '/') {
+  isRoot = true
+}
+
+const pkg = await import(`${__dirname}${workspace}/package.json`, { assert: { type: 'json' } });
+
+
+const { script } = await prompt({
+  type: 'select',
+  name: 'script',
+  message: 'Select script to run',
+  choices: Object.keys(pkg.default.scripts)
+})
+
+let cmd = `pnpm ${isRoot ? '' : `-F ${pkg.default.name}`} ${script}`
+
+execSync(cmd, { stdio: 'inherit' });
